41f39cd98525806dc9b5ddf3451e7cb2f75cef18
kaynnan <kaynnanx1@gmail.com>
2026-07-31 15:06:57 -0300

    [ADD] cgl_repair_equipment_custom: add new module

---
+++ cgl_repair_equipment_custom/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2026 Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "CGL Repair Equipment Custom",
+    "summary": "Add an Equipment field to Repair Orders",
+    "version": "18.0.1.0.0",
+    "category": "Repair",
+    "license": "AGPL-3",
+    "author": "Escodoo, Odoo Community Association (OCA)",
+    "website": "https://github.com/Escodoo/cgl-addons",
+    "depends": [
+        "repair",
+        "maintenance",
+    ],
+    "data": [
+        "views/repair_order_views.xml",
+    ],
+}
