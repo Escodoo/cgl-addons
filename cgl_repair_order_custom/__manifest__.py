b6bc175386b6d8ce10ac963adcba71946bac3015
kaynnan <kaynnanx1@gmail.com>
2026-07-31 15:06:57 -0300

    [ADD] cgl_repair_order_custom: add new module

---
+++ cgl_repair_order_custom/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2026 Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "CGL Repair Order Custom",
+    "summary": "CGL-specific view customizations for Repair Orders",
+    "version": "18.0.1.0.0",
+    "category": "Repair",
+    "license": "AGPL-3",
+    "author": "Escodoo, Odoo Community Association (OCA)",
+    "website": "https://github.com/Escodoo/cgl-addons",
+    "depends": [
+        "repair",
+    ],
+    "data": [
+        "views/repair_order_views.xml",
+    ],
+}
